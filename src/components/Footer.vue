f9f4c9d6c7b1d43c90a3aaad1bcc1a2db359b6bd
hirodashi <zacharydurland@gmail.com>
2019-03-10 17:54:22 -0500

    break out footer and integrate bootstrap

---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer class="hello">
+    <img src="../assets/logo.png" />
+    <h4>{{ siteTitle }}</h4>
+    <p>Dallas / Fort Worth Front-end Website Developer</p>
+    <p>
+      <ul>
+        <li><a href="https://github.com/hirodashi/">Github</a></li>
+        <li><a href="https://codepen.io/hirodashi">Codepen</a><li>
+        <li><a href="https://www.linkedin.com/in/zacharydurland/">LinkedIn</a></li>
+      </ul>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    siteTitle: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  max-height: 25px;
+}
+
+footer {
+  background-color: #ebebeb;
+  padding: 33px;
+}
+
+ul {
+  margin: 0 ;
+  padding: 0;
+}
+
+ul li {
+  list-style-type: none;
+  display: inline-block;
+  margin: 0 5px;
+}
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,10 @@
   <div class="hello">
     <img src="../assets/logo.png" />
     <h1>{{ siteTitle }}</h1>
-    <h2>Dallas / Fort Worth Front-end Website Developer</h2>
+    <h2>
+      Dallas / Fort Worth <br />
+      Front-end Website Developer
+    </h2>
   </div>
 </template>
 
@@ -17,18 +20,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+img {
+  margin-bottom: 15px;
 }
 </style>

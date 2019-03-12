0002028d6e4e20b65e9a73c2527c517eb8ee93a6
hirodashi <zacharydurland@gmail.com>
2019-03-11 20:56:39 -0500

    troubleshoot deploy

---
+++ src/siteData.js
@@ -0,0 +1,16 @@
+export default {
+  siteTitle: "Zachary Durland",
+  siteSubtitle: "Dallas / Fort Worth - Front-end Website Developer",
+  contact: {
+    phone: "8177035405",
+    email: "zacharydurland@gmail.com"
+  },
+  siteLogo: "logo.png",
+  places: {
+    twitter: null,
+    facebook: null,
+    linkedIn: "https://www.linkedin.com/in/zacharydurland",
+    codepen: "https://www.codepen.io/hirodashi",
+    github: "https://www.github.com/hirodashi"
+  }
+};
